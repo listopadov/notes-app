cdb90c78c61290e82edc2557d3ae4d15ee25fb7a
Roma Maslakov <maslakov.roman@itsolution.expert>
2018-03-05 00:28:28 +0200

    [ref] refactored organization of the folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const APP_ROOT = document.querySelector("#root");
+
+ReactDOM.render(<App />, APP_ROOT);
 registerServiceWorker();
