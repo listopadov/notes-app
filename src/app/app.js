39ef210c13ea417edb9409bbab9128ee2e77e255
Roma Maslakov <maslakov.roman@itsolution.expert>
2018-03-07 21:32:53 +0200

    refactores sass plugin

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../assets/img/logo.svg';
-import './app.css';
+import './app.scss';
 
 class App extends Component {
   render() {
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="app">
         <header className="app-header">
           <img src={logo} className="app-logo" alt="logo" />
-          <h1 className="app-title">Welcome to React!</h1>
+          <h1 className="app-title">Welcome to React!!!</h1>
         </header>
         <p className="app-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
